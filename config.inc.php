3def734b8c3717fc4b774d8c80a1e3a9ccd20a72
Polibio Alves <polibioalves42@gmail.com>
2025-11-05 23:24:03 -0300

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -2,8 +2,9 @@
 
     $conexao = mysqli_connect("localhost:3310","root","");
 
-    $db = mysqli_select_db($conexao,"projeto1");
+    $db = mysqli_select_db($conexao,"banco01");
 
     if(!$conexao){
        echo "<h2>Erro ao conectar o banco de dados</h2>";
+
     }
